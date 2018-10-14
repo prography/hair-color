9e207287f36e7e168f9bb03fa7bb01cce8fd27cd
sungjaehong <42910474+kogiri@users.noreply.github.com>
2018-10-14 21:08:35 +0900

    made a model

--- Hong/model.py
+++ Hong/model.py
@@ -1,36 +1,87 @@
 import torch
 import torch.nn as nn
 
+class conv_start(nn.Module):
+    def __init__(self,in_channels, out_channels):
+        super(conv_start, self).__init__()
+        self.conv = nn.Conv2d(in_channels,out_channels,kernel_size=3, stride= 2, padding=1)
 
-class Basic_CNN(nn.Module):
-    def __init__(self, im_size, nf, num_classes):
-        super(Basic_CNN, self).__init__()
-
-        self.conv1 = nn.Sequential(
-            nn.Conv2d(3, nf, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(num_features=nf),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2)
-        )
-        self.conv2 = nn.Sequential(
-            nn.Conv2d(nf, nf * 2, 3, 1, 1),
-            nn.BatchNorm2d(nf * 2),
-            nn.ReLU(),
-            nn.MaxPool2d(2, 2)
-        )
-        self.conv3 = nn.Sequential(
-            nn.Conv2d(nf * 2, nf * 4, 3, 1, 1),
-            nn.BatchNorm2d(nf * 4),
-            nn.ReLU(),
-            nn.MaxPool2d(2, 2)
-        )
-        self.fc = nn.Linear(int(im_size / 8) * int(im_size / 8) * (nf * 4), num_classes)
-
-    def forward(self, x):
-        x = self.conv1(x)
-        x = self.conv2(x)
-        x = self.conv3(x)
-        x = x.reshape(x.size(0), -1)
-        x = self.fc(x)
-
+    def forward(self,x):
+        x=self.conv(x)
         return x
+
+class encode_depthwise(nn.Module):
+    def __init__(self,in_channels, out_channels,stride):
+        super(encode_depthwise,self).__init__()
+        self.conv = nn.Squential(
+            nn.Conv2d(in_channels,in_channels,3, stride, padding=1, groups=in_channels),
+            nn.BatchNorm2d(in_channels),
+            nn,ReLU(),
+            nn.Conv2d(in_channels,out_channels,1,stride=1,padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU()
+        )
+
+    def forward(self,x):
+        x= self.conv()
+        return x
+
+class decode_depthwise(nn.Module):
+    def __init__(self,in_channels, out_channels, stride):
+        super(decode_depthwise,self).__init__()
+        self.conv = nn.Squential(
+            nn.Conv2d(in_channels, in_channels, 3, stride, padding=1, groups=in_channels),
+            nn.Conv2d(in_channels, out_channels,1,stride,padding=1,groups=1)
+            nn.ReLU()
+        )
+
+    def forward(self,x):
+        x= self.conv()
+        return x
+
+class conv_last(nn.Module):
+    def __init__(self,in_channels,out_channels):
+        super(conv_last, self).__init__()
+        self.conv = nn.Squential(
+            nn.Conv2d(in_channels,out_channels, kernel_size=1, stride=1),
+
+    def forward(self,x):
+            x=self.conv()
+            return x
+
+
+class Mobilehair(nn.Module):
+    def __init__(self):
+        super(Mobilehair, self).__init__()
+
+
+        self.inc = conv_start(3,32)
+        self.edw1 = encode_depthwise(32, 64, 1)
+        self.edw2 = encode_depthwise(64, 128, 2)
+        self.edw3 = encode_depthwise(128, 128, 1)
+        self.edw4 = encode_depthwise(128, 256, 2)
+        self.edw5 = encode_depthwise(256, 256, 1)
+        self.edw6 = encode_depthwise(256, 512, 2)
+        self.edw7 = encode_depthwise(512, 512, 1)
+        self.edw8 = encode_depthwise(512, 512, 1)
+        self.edw9 = encode_depthwise(512, 512, 1)
+        self.edw10 = encode_depthwise(512, 512, 1)
+        self.edw11 = encode_depthwise(512, 512, 1)
+        self.edw12 = encode_depthwise(512, 1024, 2)
+        self.edw13 = encode_depthwise(1024, 1024, 1)
+
+
+        self.up1 = nn.Upsample(scale_factor=2)
+        self.ddw1 = decode_depthwise(1024, 64, 1)
+        self.up2 = nn.Upsample(scale_factor=2)
+        self.ddw2 = decode_depthwise(64,64, 1)
+        self.up3 = nn.Upsample(scale_factor=2)
+        self.ddw3 = decode_depthwise(64, 64, 1)
+        self.up4 = nn.Upsample(scale_factor=2)
+        self.ddw4 = decode_depthwise(scale_factor=2)
+        self.up5 = nn.Upsamle(scale_factor=2)
+        self.ddw5 = decode_depthwise(64, 64, 1)
+        self.lac = conv_last(64,64)
+
+
+
