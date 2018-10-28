7a4ab01d351c002f68d1b8850f473b5a79e16f63
Soongja <david0801@naver.com>
2018-10-28 18:22:33 +0900

    readability 향상

--- soongja-pytorch/data_loader.py
+++ soongja-pytorch/data_loader.py
@@ -9,21 +9,22 @@ from tqdm import tqdm
 
 
 class Dataset(torch.utils.data.Dataset):
-    def __init__(self, data_dir, image_size):
-        self.data_dir = data_dir
-        if not os.path.exists(self.data_dir):
-            raise Exception("[!] {} not exists.".format(self.data_dir))
+    def __init__(self, folder, image_size):
+        self.folder = folder
+        self.image_size = image_size
 
-        self.image_paths = os.listdir(os.path.join(self.data_dir, 'images')) #파일명은 같기에 "image"대신 "mask"해도 동일합니다.
+        if not os.path.exists(self.folder):
+            raise Exception("[!] {} not exists.".format(self.folder))
+
+        self.image_paths = os.listdir(os.path.join(self.folder, 'images')) #파일명은 같기에 "image"대신 "mask"해도 동일.
         print('Dataset size:', len(self.image_paths))
+
         if len(self.image_paths) == 0:
             raise Exception("No images are found in {}".format(self.data_dir))
 
-        self.image_size = image_size
-
     def __getitem__(self, index):
-        image = Image.open(os.path.join(self.data_dir, 'images', self.image_paths[index])).convert('RGB')
-        mask = Image.open(os.path.join(self.data_dir, 'masks', self.image_paths[index]))
+        image = Image.open(os.path.join(self.folder, 'images', self.image_paths[index])).convert('RGB')
+        mask = Image.open(os.path.join(self.folder, 'masks', self.image_paths[index]))
 
         image = image.resize((self.image_size, self.image_size))
         mask = mask.resize((self.image_size, self.image_size))
@@ -43,7 +44,7 @@ class Dataset(torch.utils.data.Dataset):
         return len(self.image_paths)
 
 
-def get_loader(data_dir, batch_size, image_size, shuffle, num_workers):
+def get_loader(data_dir, image_size, batch_size, shuffle, num_workers):
     dataset = Dataset(data_dir, image_size)
 
     dataloader = torch.utils.data.DataLoader(dataset=dataset,
--- soongja-pytorch/main.py
+++ soongja-pytorch/main.py
@@ -9,9 +9,6 @@ from train import Trainer
 
 
 def main(config):
-    os.makedirs(config.sample_dir, exist_ok=True)
-    os.makedirs(config.checkpoint_dir, exist_ok=True)
-
     config.manual_seed = random.randint(1, 10000)
     print("Random Seed: ", config.manual_seed)
     random.seed(config.manual_seed)
@@ -22,11 +19,15 @@ def main(config):
 
     cudnn.benchmark = True
 
-    dataloader = get_loader(config.data_dir, config.batch_size, config.image_size,
+    train_loader = get_loader(config.data_dir, config.image_size, config.batch_size,
                             shuffle=True, num_workers=int(config.workers))
 
-    trainer = Trainer(config, dataloader)
+    test_loader = get_loader(config.test_data_dir, config.image_size, batch_size=1,
+                             shuffle=False, num_workers=int(config.workers))
+
+    trainer = Trainer(config, train_loader, test_loader)
     trainer.train()
+
 
 if __name__ == "__main__":
     config = get_config()
