c6ecfab45b08014d1a2b1f5760fe2348b6230edb
Soongja <david0801@naver.com>
2018-10-29 19:10:43 +0900

    logits는 굳이 찍어볼 필요 없어서 삭제

--- soongja-tensorflow/utils.py
+++ soongja-tensorflow/utils.py
@@ -9,16 +9,14 @@ def show_all_variables():
   slim.model_analyzer.analyze_vars(model_vars, print_info=True)
 
 
-def draw_results(IOU, inputs, targets, logits_a, logits_b, preds,
+def draw_results(IOU, inputs, targets, preds,
                  epoch, batch_num, sample_dir, model_dir, num_samples):
-    fig, axs = plt.subplots(5, num_samples, figsize=(num_samples * 3, 10))
+    fig, axs = plt.subplots(3, num_samples, figsize=(num_samples * 3, 10))
     fig.suptitle("IOU: %.4f" % IOU, fontsize=20)
     for example_i in range(num_samples):
         axs[0][example_i].imshow(inputs[example_i])
         axs[1][example_i].imshow(targets[example_i], cmap='gray')
-        axs[2][example_i].imshow(logits_a[example_i], cmap='gray')
-        axs[3][example_i].imshow(logits_b[example_i], cmap='gray')
-        axs[4][example_i].imshow(preds[example_i], cmap='gray')
+        axs[2][example_i].imshow(preds[example_i], cmap='gray')
 
     sample_dir = os.path.join(sample_dir, model_dir)
     if not os.path.exists(sample_dir):
