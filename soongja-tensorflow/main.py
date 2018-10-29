e9b6abe6b8a9c8ab1000e7530263e8f10b050975
Soongja <david0801@naver.com>
2018-10-29 19:15:04 +0900

    prefix 달 수 있게 함

--- soongja-tensorflow/main.py
+++ soongja-tensorflow/main.py
@@ -1,4 +1,3 @@
-import os
 import tensorflow as tf
 from model import MobileHairNet
 from utils import show_all_variables
@@ -7,14 +6,14 @@ flags = tf.app.flags
 flags.DEFINE_integer("input_height", 224, "The height of input image")
 flags.DEFINE_integer("input_width", 224, "The width of input image")
 flags.DEFINE_integer("batch_size", 4, "The size of image batches")
-flags.DEFINE_integer("epoch", 20, "Epochs to train")
+flags.DEFINE_integer("epoch", 50, "Epochs to train")
 flags.DEFINE_float("learning_rate", 0.001, "Learning rate for Adam")
 flags.DEFINE_string("graph_dir", "graphs", "Directory name to save graphs")
 flags.DEFINE_string("log_dir", "logs", "Directory name to save tensorboard summaries")
 flags.DEFINE_string("checkpoint_dir", "checkpoints", "Directory name to save checkpoints")
 flags.DEFINE_string("sample_dir", "samples", "Directory name to save validation image samples")
-flags.DEFINE_string("data_dir", "dataset/hairdata", "Root directory of your dataset(Don't use . or /")
-flags.DEFINE_string("data_name", "hairdata", "Used for folder names of ckpts, logs, etc.(do not use . or /")
+flags.DEFINE_string("data_dir", "dataset/hairdata", "Root directory of your dataset")
+flags.DEFINE_string("prefix", "Mon", "Used for folder names of ckpts, logs, etc.(do not use . or /")
 flags.DEFINE_integer("validation_step", 200, "Step Interval to test on validation set")
 
 FLAGS = flags.FLAGS
@@ -30,6 +29,7 @@ def main(_):
         show_all_variables()
 
         model.train()
+
 
 if __name__ == '__main__':
     tf.app.run()
