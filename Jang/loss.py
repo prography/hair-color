edit wrong point in gradient loss

Jang/loss.py
@@ -18,7 +18,7 @@ def image_gradient(image):
 def image_gradient_loss(image, pred):
     loss = 0
     for i in range(len(image)):
-        pred_grad_x, pred_grad_y = image_gradient(pred[i][0].cpu())
+        pred_grad_x, pred_grad_y = image_gradient(pred[i][0].cpu().detach().numpy())
         gray_image = torch.from_numpy(rgb2gray(image[i].permute(1, 2, 0).cpu().numpy()))
         image_grad_x, image_grad_y = image_gradient(gray_image)
         IMx = torch.mul(image_grad_x, pred_grad_x)
@@ -45,7 +45,7 @@ class HairMatLoss(_WeightedLoss):
         mask_flat = mask.squeeze(1).view(-1).long()
         cross_entropy_loss = F.cross_entropy(pred_flat, mask_flat, weight=self.weight
                                              , ignore_index=self.ignore_index, reduction=self.reduction)
-        image_loss = image_gradient_loss(image, mask).to(self.device)
+        image_loss = image_gradient_loss(image, pred).to(self.device)
         return torch.add(cross_entropy_loss, 0.5*image_loss.float())
 
 def iou_loss(pred, mask):
@@ -55,3 +55,34 @@ def iou_loss(pred, mask):
     Overlep = torch.mul(pred, mask)
     loss = torch.div(torch.sum(Overlep).float(), torch.sum(Union).float())
     return loss
+
+"""
+class ImageGradientLoss:
+    def __init__(self, image, mask):
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        self.image = image
+        self.mask = mask
+    def get_loss(self):
+        image_grad_x, image_grad_y = ImageGradient(self.image).get_gradient()
+        mask_grad_x, mask_grad_y = ImageGradient(self.image).get_gradient()
+        IMx = torch.mul(image_grad_x, mask_grad_x)
+        IMy = torch.mul(image_grad_y, mask_grad_y)
+        Mmag = torch.sqrt(torch.add(torch.pow(mask_grad_x, 2), torch.pow(mask_grad_y, 2)))
+        IM = torch.add(1, torch.neg(torch.add(IMx, IMy)))
+        numerator = torch.sum(torch.mul(Mmag, IM))
+        denominator = torch.sum(Mmag)
+        out = torch.div(numerator, denominator)
+        return torch.FloatTensor(out)
+
+class HairMatLoss:
+    def __init__(self):
+        self.num_classes = config.num_classes
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        self.CrossEntropyLoss = nn.CrossEntropyLoss().to(self.device)
+    def get_loss(self, pred, mask):
+        pred_flat = pred.permute(0, 2, 3, 1).contiguous().view(-1, self.num_classes)
+        mask_flat = mask.squeeze(1).view(-1).long()
+        criterion = self.CrossEntropyLoss(pred_flat, mask_flat)
+        grad_loss = ImageGradientLoss(pred.cpu, mask.cpu).get_loss()
+        return torch.add(grad_loss, criterion)
+"""