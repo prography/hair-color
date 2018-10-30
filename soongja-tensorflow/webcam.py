54931db4a84965205212641edc8def922b69cb7a
Soongja <david0801@naver.com>
2018-10-30 18:50:12 +0900

    webcam color trackbar 추가

--- soongja-tensorflow/webcam.py
+++ soongja-tensorflow/webcam.py
@@ -12,19 +12,21 @@ flags.DEFINE_string("checkpoint_dir", "checkpoints/hairdata_4_224_224", "Directo
 FLAGS = flags.FLAGS
 
 
-def alpha_blend(frame, mask):
-
-    color = [0, 130, 180]
+def alpha_blend(frame, mask, b, g, r):
 
     colored_mask = np.zeros((mask.shape[0], mask.shape[1], 3), np.uint8)
-    colored_mask[np.where(mask != 0)] = color # BGR!!!
-    foreground = (0.75 * frame + 0.2 * colored_mask).astype(np.uint8)
+    colored_mask[np.where(mask != 0)] = [b, g, r] # BGR!!!
+    foreground = (0.5 * frame + 0.5 * colored_mask).astype(np.uint8)
     alpha_hand = cv2.bitwise_and(foreground, foreground, mask=mask)
 
     mask_inv = cv2.bitwise_not(mask)
     background = cv2.bitwise_and(frame, frame, mask=mask_inv)
 
     return cv2.add(alpha_hand, background)
+
+
+def nothing(x):
+    pass
 
 
 def main(_):
@@ -40,6 +42,15 @@ def main(_):
             print('Load success: %s' % ckpt.model_checkpoint_path)
         else:
             print('Load failed. Check your checkpoint directory!!!')
+
+        # color
+        palette = np.zeros((120, 360, 3), np.uint8)
+        cv2.namedWindow('demo', cv2.WINDOW_NORMAL)
+        # cv2.namedWindow('palette', cv2.WINDOW_NORMAL)
+
+        cv2.createTrackbar('R', 'demo', 0, 255, nothing)
+        cv2.createTrackbar('G', 'demo', 0, 255, nothing)
+        cv2.createTrackbar('B', 'demo', 0, 255, nothing)
 
         cap = cv2.VideoCapture(0)
         frames = 0
@@ -58,13 +69,23 @@ def main(_):
                 mask = sess.run(net.preds, feed_dict={net.inputs: _input})
                 mask = mask[0].astype(np.uint8) * 255
                 mask = cv2.resize(mask, (360, 360))
+                _, mask = cv2.threshold(mask, 200, 255, cv2.THRESH_BINARY)
 
-                out = alpha_blend(frame, mask)
+                r = cv2.getTrackbarPos('R', 'demo')
+                g = cv2.getTrackbarPos('G', 'demo')
+                b = cv2.getTrackbarPos('B', 'demo')
+
+                palette[:] = [b, g, r]
+
+                out = alpha_blend(frame, mask, b, g, r)
+
+                out = np.vstack((out, palette))
 
                 frames += 1
                 fps = frames / (time.time() - start_time)
                 cv2.putText(out, 'FPS: %.2f' % fps, (10, 20), cv2.FONT_HERSHEY_PLAIN, 1, [0, 255, 0], 1)
 
+                # cv2.imshow('palette', palette)
                 cv2.imshow('demo', out)
                 key = cv2.waitKey(1)
                 if key & 0xFF == ord('q'):
