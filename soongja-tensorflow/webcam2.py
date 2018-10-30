76292b6dd3a319eeb6c29e12d075da6efe5f8429
Soongja <david0801@naver.com>
2018-10-30 20:43:40 +0900

    webcam.py: with opencv trackbar webcam2.py: with pyqt color picker

--- soongja-tensorflow/webcam2.py
+++ soongja-tensorflow/webcam2.py
@@ -2,6 +2,8 @@ import time
 import cv2
 import numpy as np
 import tensorflow as tf
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QColorDialog
+from PyQt5.QtCore import pyqtSlot
 from model import Network
 
 flags = tf.app.flags
@@ -12,11 +14,54 @@ flags.DEFINE_string("checkpoint_dir", "checkpoints/hairdata_4_224_224", "Directo
 FLAGS = flags.FLAGS
 
 
-def alpha_blend(frame, mask, b, g, r):
+class App(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.title = 'hair color picker'
+        self.left = 200
+        self.top = 200
+        self.width = 200
+        self.height = 80
+        self.button_left = 55
+        self.button_top = 30
+        self.rgb = (0, 0, 0) # initial value
+        self.initUI()
 
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+
+        button = QPushButton('Choose Color!', self)
+        button.move(self.button_left, self.button_top)
+        button.clicked.connect(self.on_click)
+
+        self.show()
+
+    @pyqtSlot()
+    def on_click(self):
+        self.rgb = openColorDialog(self)
+
+
+def hex_to_rgb(value):
+    value = value.lstrip('#')
+    lv = len(value)
+    return tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
+
+
+def openColorDialog(self):
+    color = QColorDialog.getColor()
+    return hex_to_rgb(color.name())
+
+
+def alpha_blend(frame, mask, rgb, ratio):
+    '''
+    :param rgb: a tuple with 3 elements (r, g, b)
+    :param ratio: a tuple with 2 elements (frame_alpha, color_alpha)
+    :return: opencv frame alpha blended
+    '''
     colored_mask = np.zeros((mask.shape[0], mask.shape[1], 3), np.uint8)
-    colored_mask[np.where(mask != 0)] = [b, g, r] # BGR!!!
-    foreground = (0.9 * frame + 0.1 * colored_mask).astype(np.uint8)
+    colored_mask[np.where(mask != 0)] = [rgb[2], rgb[1], rgb[0]] # BGR!!!
+    foreground = (ratio[0] * frame + ratio[1] * colored_mask).astype(np.uint8)
     alpha_hand = cv2.bitwise_and(foreground, foreground, mask=mask)
 
     mask_inv = cv2.bitwise_not(mask)
@@ -25,11 +70,10 @@ def alpha_blend(frame, mask, b, g, r):
     return cv2.add(alpha_hand, background)
 
 
-def nothing(x):
-    pass
-
-
 def main(_):
+    app = QApplication([])
+    qt = App()
+
     net = Network(FLAGS.input_height, FLAGS.input_width)
 
     with tf.Session() as sess:
@@ -42,15 +86,6 @@ def main(_):
             print('Load success: %s' % ckpt.model_checkpoint_path)
         else:
             print('Load failed. Check your checkpoint directory!!!')
-
-        # color
-        palette = np.zeros((120, 360, 3), np.uint8)
-        cv2.namedWindow('demo', cv2.WINDOW_NORMAL)
-        # cv2.namedWindow('palette', cv2.WINDOW_NORMAL)
-
-        cv2.createTrackbar('R', 'demo', 0, 255, nothing)
-        cv2.createTrackbar('G', 'demo', 0, 255, nothing)
-        cv2.createTrackbar('B', 'demo', 0, 255, nothing)
 
         cap = cv2.VideoCapture(0)
         frames = 0
@@ -71,21 +106,12 @@ def main(_):
                 mask = cv2.resize(mask, (360, 360))
                 _, mask = cv2.threshold(mask, 200, 255, cv2.THRESH_BINARY)
 
-                r = cv2.getTrackbarPos('R', 'demo')
-                g = cv2.getTrackbarPos('G', 'demo')
-                b = cv2.getTrackbarPos('B', 'demo')
-
-                palette[:] = [b, g, r]
-
-                out = alpha_blend(frame, mask, b, g, r)
-
-                out = np.vstack((out, palette))
+                out = alpha_blend(frame, mask, qt.rgb, ratio=(0.9, 0.1))
 
                 frames += 1
                 fps = frames / (time.time() - start_time)
                 cv2.putText(out, 'FPS: %.2f' % fps, (10, 20), cv2.FONT_HERSHEY_PLAIN, 1, [0, 255, 0], 1)
 
-                # cv2.imshow('palette', palette)
                 cv2.imshow('demo', out)
                 key = cv2.waitKey(1)
                 if key & 0xFF == 27:
