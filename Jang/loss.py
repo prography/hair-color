a5bd703dc3c4a97acdc818f2c49e85bc338a4c84
Jang Wonbeom <jtiger958@gmail.com>
2018-10-31 19:19:48 +0900

    update webcam.py

--- Jang/loss.py
+++ Jang/loss.py
@@ -56,34 +56,3 @@ def iou_loss(pred, mask):
     Overlep = torch.mul(pred, mask)
     loss = torch.div(torch.sum(Overlep).float(), torch.sum(Union).float())
     return loss
-
-"""
-class ImageGradientLoss:
-    def __init__(self, image, mask):
-        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-        self.image = image
-        self.mask = mask
-    def get_loss(self):
-        image_grad_x, image_grad_y = ImageGradient(self.image).get_gradient()
-        mask_grad_x, mask_grad_y = ImageGradient(self.image).get_gradient()
-        IMx = torch.mul(image_grad_x, mask_grad_x)
-        IMy = torch.mul(image_grad_y, mask_grad_y)
-        Mmag = torch.sqrt(torch.add(torch.pow(mask_grad_x, 2), torch.pow(mask_grad_y, 2)))
-        IM = torch.add(1, torch.neg(torch.add(IMx, IMy)))
-        numerator = torch.sum(torch.mul(Mmag, IM))
-        denominator = torch.sum(Mmag)
-        out = torch.div(numerator, denominator)
-        return torch.FloatTensor(out)
-
-class HairMatLoss:
-    def __init__(self):
-        self.num_classes = config.num_classes
-        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-        self.CrossEntropyLoss = nn.CrossEntropyLoss().to(self.device)
-    def get_loss(self, pred, mask):
-        pred_flat = pred.permute(0, 2, 3, 1).contiguous().view(-1, self.num_classes)
-        mask_flat = mask.squeeze(1).view(-1).long()
-        criterion = self.CrossEntropyLoss(pred_flat, mask_flat)
-        grad_loss = ImageGradientLoss(pred.cpu, mask.cpu).get_loss()
-        return torch.add(grad_loss, criterion)
-"""
